2dbc364b17240ca0ed4d6fc131a6fa985a845af1
cedric karungu <ckarungu921@gmail.com>
2024-02-02 11:58:53 +0200

    feat: populate related data (user - setting)

---
+++ src/__mocks__/mockSettings.ts
@@ -0,0 +1,20 @@
+export const settings = [
+  {
+    id: 1,
+    userId: 2,
+    receiveNotifications: true,
+    receiveMail: false,
+  },
+  {
+    id: 2,
+    userId: 1,
+    receiveNotifications: true,
+    receiveMail: true,
+  },
+  {
+    id: 1,
+    userId: 3,
+    receiveNotifications: false,
+    receiveMail: false,
+  },
+];
--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,8 +1,17 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { User } from '../models/User';
 import { users } from 'src/__mocks__/mocksUsers';
+import { UserSetting } from '../models/UserSetting';
+import { settings } from 'src/__mocks__/mockSettings';
 
-@Resolver()
+@Resolver((of) => User)
 export class UserResolver {
   @Query(() => User, { nullable: true })
   getUserById(@Args('id', { type: () => Int }) id: number) {
@@ -12,5 +21,10 @@ export class UserResolver {
   @Query(() => [User])
   getUsers() {
     return users;
+  }
+
+  @ResolveField(() => UserSetting, { name: 'settings', nullable: true })
+  getUserSettings(@Parent() user: User) {
+    return settings.find((stn) => stn.userId === user.id);
   }
 }
