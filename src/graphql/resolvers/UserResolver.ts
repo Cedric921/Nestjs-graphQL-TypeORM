21fa8dfc1499d6ee91118a2b5928ab90365db502
cedric karungu <ckarungu921@gmail.com>
2024-02-02 13:21:46 +0200

    Mutation

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,6 +1,7 @@
 import {
   Args,
   Int,
+  Mutation,
   Parent,
   Query,
   ResolveField,
@@ -10,6 +11,8 @@ import { User } from '../models/User';
 import { users } from 'src/__mocks__/mocksUsers';
 import { UserSetting } from '../models/UserSetting';
 import { settings } from 'src/__mocks__/mockSettings';
+
+export let incrementId = users.length;
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -26,5 +29,17 @@ export class UserResolver {
   @ResolveField(() => UserSetting, { name: 'settings', nullable: true })
   getUserSettings(@Parent() user: User) {
     return settings.find((stn) => stn.userId === user.id);
+  }
+
+  // Mutations
+  @Mutation(() => User)
+  createUser(
+    @Args('username') username: string,
+    @Args('displayName', { nullable: true }) displayName: string,
+  ) {
+    //
+    const newUser = { username, displayName, id: ++incrementId };
+    users.push(newUser);
+    return newUser;
   }
 }
